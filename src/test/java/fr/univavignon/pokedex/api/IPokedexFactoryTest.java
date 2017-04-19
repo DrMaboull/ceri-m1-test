a0727376aa57107ead62db28b465cad4b30dea1d
PlateformeDebat <remclemat03@hotmail.fr>
2017-04-19 15:57:27 +0200

    hehe

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,5 +1,52 @@
 package fr.univavignon.pokedex.api;
 
-public class IPokedexFactoryTest {
+import static org.junit.Assert.assertEquals;
 
+import java.awt.List;
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+public class IPokedexFactoryTest {
+	
+	@Mock
+	public IPokedexFactory PokedexFactory;
+	
+	private IPokemonMetadataProvider provider;
+	private IPokemonFactory factory;
+	private IPokedex Poke;
+	private Pokemon bulbizarre = new Pokemon(0, "Bulbi", 613, 64, 4000, 4, 126, 126, 90, 56);
+	private ArrayList<Pokemon> test = new ArrayList() {{ add(bulbizarre); }};
+	
+	
+	@Before
+	public void setUp() throws PokedexException
+	{
+		MockitoAnnotations.initMocks(this);
+		Mockito.when(PokedexFactory.createPokedex(provider, factory)).thenReturn(Poke);
+		Mockito.when(PokedexFactory.createPokedex(provider, factory).getPokemon(0)).thenReturn(bulbizarre);
+		Mockito.when(PokedexFactory.createPokedex(provider, factory).getPokemons()).thenReturn(test);
+		Mockito.when(PokedexFactory.createPokedex(provider, factory).size()).thenReturn(1);
+		Mockito.when(PokedexFactory.createPokedex(provider, factory).addPokemon(bulbizarre)).thenReturn(2);
+	}
+	
+	@Test
+	public void createPokedexTest() throws PokedexException
+	{
+		assertEquals(PokedexFactory.createPokedex(provider, factory).getPokemons(), Poke.getPokemons());
+		
+			try {
+				assertEquals(PokedexFactory.createPokedex(provider, factory).getPokemon(0), Poke.getPokemon(0));
+			} catch (PokedexException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		assertEquals(PokedexFactory.createPokedex(provider, factory).size(), Poke.size());
+		assertEquals(PokedexFactory.createPokedex(provider,factory).addPokemon(bulbizarre), Poke.addPokemon(bulbizarre));
+	}
 }
